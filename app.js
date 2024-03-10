f94472c22de2c92f0a1590e354cbae0dd414732e
Jimmy <skawk3235@gmail.com>
2024-03-10 23:00:36 +0900

    session 사용 설정

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 require('dotenv').config();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    name: 'isSigned',
+  })
+);
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
